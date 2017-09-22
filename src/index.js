1fcbdf390e1b32a3ac8eaaf82de02086ed3c2432
OkYesAnap <OkYesAnap@gmail.com>
2017-09-22 12:51:29 +0300

    Quallium CRUD

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import './theme/index.css';
-import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom'
+import './index.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+
 const render = Component => ReactDOM.render(
-    <Router>
-        <App />
-    </Router>, document.getElementById('root'));
+  <Router>
+      <App />
+  </Router>, document.getElementById('root'));
 render(App);
 
-if(module.hot){
-    console.log('Updated');
-    module.hot.accept('./App', () => render(App));
-}
 registerServiceWorker();
